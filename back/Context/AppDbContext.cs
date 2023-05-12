c785e8f635081ea3af237b98c1fb6047c95ae291
Ren-9 <braianmartinez648@gmail.com>
2023-05-12 15:57:00 -0400

    feat: add packages

---
+++ back/Context/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    //public DbSet<Class> Plate { get; set; }
+}
---
+++ back/Startup.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+public class Startup
+{
+    private readonly string myCors = "";
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy(name: myCors, builder =>
+            {
+                builder.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost").AllowAnyHeader().AllowAnyMethod();
+            });
+        });
+
+        services.AddControllers();
+
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "P16API", Version = "v1" });
+        });
+        services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
+                 Configuration.GetConnectionString("connectionString")));
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "P16API v1"));
+        }
+
+        app.UseRouting();
+        app.UseCors(myCors);
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
