7fd01af705e1dcffc7e131eb8aa66d15275c1bf3
josias jairo <jairo.josias14prueba.com>
2023-05-15 17:50:07 -0500

    feat:add ranking,social class

---
+++ back/Models/Ranking.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace back.Models;
+
+public class Ranking
+{
+    [Key]
+    public int Id { get; set; }
+    public int Sum { get; set; }    
+
+    public int Count { get; set; }  
+
+}
---
+++ back/Models/Social.cs
@@ -0,0 +1,12 @@
+﻿namespace back.Models;
+
+public class Social
+{
+    public int Id { get; set; } 
+    public string Facebook { get; set; }
+
+    public string instagram { get; set; }
+
+
+
+}
