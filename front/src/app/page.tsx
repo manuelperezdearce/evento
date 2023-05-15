94cc96eb47b0e00e7c2ef457efd69871a2087a4e
Ren-9 <braianmartinez648@gmail.com>
2023-05-15 18:20:49 -0400

    feat: add description in Home

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,10 +1,16 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from 'next/image';
+import styles from './page.module.css';
 
-export default function Home () {
+export default function Home() {
   return (
     <main className={styles.main}>
       <h1>hello no country</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est iure culpa
+        delectus magnam ipsa, optio reprehenderit voluptate reiciendis hic
+        veniam nemo quia velit fuga cupiditate. Facere veniam molestias eos
+        voluptatum.
+      </p>
     </main>
-  )
+  );
 }
