732ec4cb6ed79349b0146351539ef13679abc999
josias jairo <jairo.josias14prueba.com>
2023-05-16 17:30:36 -0500

    feat:add controller , services , funcionalidad

---
+++ back/Controllers/CategoryController.cs
@@ -0,0 +1,34 @@
+﻿using back.Dtos.Category;
+using back.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace back.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class CategoryController : ControllerBase
+{
+    private readonly CategoryService _categoryService;
+
+    public CategoryController(CategoryService categoryService)
+    {
+
+        _categoryService = categoryService;
+
+    }
+
+
+    [HttpPost]
+    public async Task<IActionResult> create(CategoryDto categoryDto)
+    {
+        var response = await _categoryService.create(categoryDto);
+        if (response is null)
+        
+            return BadRequest("error");
+           return Ok();
+        
+
+    }
+
+
+}
