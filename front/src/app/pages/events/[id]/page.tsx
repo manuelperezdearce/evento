b2addc7240a339fe356202ecc1ac4fb6bccfe885
Hector Barazorda <ph.barazorda@gmail.com>
2023-05-17 15:55:31 -0500

    🐛 fix: routes and divide task in EventDetails

--- front/src/app/pages/events/[id]/page.tsx
+++ front/src/app/pages/events/[id]/page.tsx
@@ -8,6 +8,9 @@ export default function EventDetail () {
   return (
     <Container>
       <h1>event with id:{id}</h1>
+      <div className='infoEvent'>info</div>
+      <div className='addressEvent'>ubicacion</div>
+      <div className='comments'>comments</div>
     </Container>
   )
 }
