9411c5a91532ceb48945b8239acd8692bcabb155
Ren-9 <braianmartinez648@gmail.com>
2023-05-18 22:13:12 +0100

    fix

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,33 +1,32 @@
-'use client'
-import { Hero } from "./common/components";
-import { EventCard } from "./common/components/EventCard";
-import SliderContainer from "./pages/home/components/SliderContainer";
+'use client';
+import { Container } from '@mui/material';
+import { Hero } from './common/components';
+import { EventCard } from './common/components/EventCard';
+import SliderContainer from './pages/home/components/SliderContainer';
 
 const sectionArrays = [
-  { title: "Panoramas Hoy" },
-  { title: "Destacados" },
-  { title: "Próximos 7 días" }
-]
+  { title: 'Panoramas Hoy' },
+  { title: 'Destacados' },
+  { title: 'Próximos 7 días' },
+];
 
 export default function Home() {
   return (
     <main>
-      <Hero />
-      {
-        sectionArrays.map((item) => {
+      <Container maxWidth="lg" sx={{ mt: '3rem', mb: '3rem' }}>
+        <Hero />
+        {sectionArrays.map((item) => {
           return (
-            <SliderContainer sliderTitle={item.title} >
-              {
-                Array(4).fill(0).map((item) => {
-                  return (
-                    <EventCard key={item} />
-                  )
-                })
-              }
+            <SliderContainer sliderTitle={item.title}>
+              {Array(4)
+                .fill(0)
+                .map((item) => {
+                  return <EventCard key={item} />;
+                })}
             </SliderContainer>
-          )
-        })
-      }
+          );
+        })}
+      </Container>
     </main>
   );
 }
--- front/src/app/pages/explore/page.tsx
+++ front/src/app/pages/explore/page.tsx
@@ -5,19 +5,19 @@ import { EventCard } from '../../common/components/EventCard';
 import { EventsContainer } from './components/EventsContainer';
 
 export default function Explore() {
-    return (
-        <Container maxWidth="lg" sx={{ mt: '3rem' }}>
-            <EventSearch />
+  return (
+    <Container maxWidth="lg" sx={{ mt: '3rem', mb: '3rem' }}>
+      <EventSearch />
 
-            <EventsContainer>
-                {Array(8)
-                    .fill(0)
-                    .map((item, index) => (
-                        <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
-                            <EventCard key={index} />
-                        </Grid>
-                    ))}
-            </EventsContainer>
-        </Container>
-    );
+      <EventsContainer>
+        {Array(8)
+          .fill(0)
+          .map((item, index) => (
+            <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+              <EventCard key={index} />
+            </Grid>
+          ))}
+      </EventsContainer>
+    </Container>
+  );
 }
