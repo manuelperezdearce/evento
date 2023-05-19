4d89fb578e2a883ebab219de6ddb8dd1745c7f74
Hector Barazorda <ph.barazorda@gmail.com>
2023-05-19 12:01:07 -0500

    🐛 fix: some errors in consola

--- front/src/app/common/components/Navbar.tsx
+++ front/src/app/common/components/Navbar.tsx
@@ -1,177 +1,189 @@
-'use client';
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
-import Link from 'next/link';
+"use client";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
+import { useEffect, useState } from "react";
+import { Link } from "@mui/material";
 
 const pages = [
-	{ name: 'Home', route: '/' },
-	{ name: 'Explorar', route: 'pages/explore' },
-	{ name: 'Nosotros', route: 'pages/about' },
+  { name: "Home", route: "/" },
+  { name: "Explorar", route: "pages/explore" },
+  { name: "Nosotros", route: "pages/about" },
 ];
 const settings = [
-	{ name: 'Perfil', route: 'pages/users' },
-	{ name: 'Logout', route: 'pages/auth' },
+  { name: "Perfil", route: "pages/users" },
+  { name: "Logout", route: "pages/auth" },
 ];
 
 export const Navbar = () => {
-	const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
-	const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+  const [domLoaded, setDomLoaded] = useState(false);
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
 
-	const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-		setAnchorElNav(event.currentTarget);
-	};
-	const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-		setAnchorElUser(event.currentTarget);
-	};
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
 
-	const handleCloseNavMenu = () => {
-		setAnchorElNav(null);
-	};
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
-	const handleCloseUserMenu = () => {
-		setAnchorElUser(null);
-	};
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
-	return (
-		<AppBar position="static">
-			<Container maxWidth="xl">
-				<Toolbar disableGutters>
-					<Link href={'/'} style={{ display: 'flex' }}>
-						<AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-						<Typography
-							variant="h6"
-							noWrap
-							component="a"
-							sx={{
-								mr: 2,
-								display: { xs: 'none', md: 'flex' },
-								fontFamily: 'monospace',
-								fontWeight: 700,
-								letterSpacing: '.3rem',
-								color: 'inherit',
-								textDecoration: 'none',
-							}}
-						>
-							LOGO
-						</Typography>
-					</Link>
+  useEffect(() => {
+    setDomLoaded(true);
+  }, []);
 
-					<Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-						<IconButton
-							size="large"
-							aria-label="account of current user"
-							aria-controls="menu-appbar"
-							aria-haspopup="true"
-							onClick={handleOpenNavMenu}
-							color="inherit"
-						>
-							<MenuIcon />
-						</IconButton>
-						<Menu
-							id="menu-appbar"
-							anchorEl={anchorElNav}
-							anchorOrigin={{
-								vertical: 'bottom',
-								horizontal: 'left',
-							}}
-							keepMounted
-							transformOrigin={{
-								vertical: 'top',
-								horizontal: 'center',
-							}}
-							open={Boolean(anchorElNav)}
-							onClose={handleCloseNavMenu}
-							sx={{
-								display: { xs: 'block', md: 'none' },
-							}}
-						>
-							{pages.map(({ name, route }) => (
-								<Link href={route || ''} key={'name'}>
-									<MenuItem key={name} onClick={handleCloseNavMenu}>
-										<Typography textAlign="center">{name}</Typography>
-									</MenuItem>
-								</Link>
-							))}
-						</Menu>
-					</Box>
-					<AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-					<Typography
-						variant="h5"
-						noWrap
-						component="a"
-						href=""
-						sx={{
-							mr: 2,
-							display: { xs: 'flex', md: 'none' },
-							flexGrow: 1,
-							fontFamily: 'monospace',
-							fontWeight: 700,
-							letterSpacing: '.3rem',
-							color: 'inherit',
-							textDecoration: 'none',
-						}}
-					>
-						LOGO
-					</Typography>
-					<Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-						{pages.map(({ name, route }) => (
-							<Link href={route || ''}>
-								<Button
-									key={name}
-									onClick={handleCloseNavMenu}
-									sx={{ my: 2, color: 'white', display: 'block' }}
-								>
-									{name}
-								</Button>
-							</Link>
-						))}
-					</Box>
+  return (
+    <>
+      {domLoaded && (
+        <AppBar position="static">
+          <Container maxWidth="xl">
+            <Toolbar disableGutters>
+              <Link href={"/"} style={{ display: "flex" }}>
+                <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+                <Typography
+                  variant="h6"
+                  noWrap
+                  component="a"
+                  sx={{
+                    mr: 2,
+                    display: { xs: "none", md: "flex" },
+                    fontFamily: "monospace",
+                    fontWeight: 700,
+                    letterSpacing: ".3rem",
+                    color: "inherit",
+                    textDecoration: "none",
+                  }}
+                >
+                  LOGO
+                </Typography>
+              </Link>
 
-					<Box sx={{ flexGrow: 0 }}>
-						<Tooltip title="Open settings">
-							<IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-								<Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-							</IconButton>
-						</Tooltip>
-						<Menu
-							sx={{ mt: '45px' }}
-							id="menu-appbar"
-							anchorEl={anchorElUser}
-							anchorOrigin={{
-								vertical: 'top',
-								horizontal: 'right',
-							}}
-							keepMounted
-							transformOrigin={{
-								vertical: 'top',
-								horizontal: 'right',
-							}}
-							open={Boolean(anchorElUser)}
-							onClose={handleCloseUserMenu}
-						>
-							{settings.map(({ name, route }) => (
-								<Link href={`${name !== 'Perfil' ? route : `${route}/1`}`}>
-									<MenuItem key={route} onClick={handleCloseUserMenu}>
-										<Typography textAlign="center">{name}</Typography>
-									</MenuItem>
-								</Link>
-							))}
-						</Menu>
-					</Box>
-				</Toolbar>
-			</Container>
-		</AppBar>
-	);
+              <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleOpenNavMenu}
+                  color="inherit"
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorElNav}
+                  anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "left",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "center",
+                  }}
+                  open={Boolean(anchorElNav)}
+                  onClose={handleCloseNavMenu}
+                  sx={{
+                    display: { xs: "block", md: "none" },
+                  }}
+                >
+                  {pages.map(({ name, route }) => (
+                    <Link href={route} key={name}>
+                      <MenuItem key={name} onClick={handleCloseNavMenu}>
+                        <Typography textAlign="center">{name}</Typography>
+                      </MenuItem>
+                    </Link>
+                  ))}
+                </Menu>
+              </Box>
+              <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+              <Typography
+                variant="h5"
+                noWrap
+                component="a"
+                href=""
+                sx={{
+                  mr: 2,
+                  display: { xs: "flex", md: "none" },
+                  flexGrow: 1,
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  color: "inherit",
+                  textDecoration: "none",
+                }}
+              >
+                LOGO
+              </Typography>
+              <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+                {pages.map(({ name, route }) => (
+                  <Link href={route} key={name}>
+                    <Button
+                      key={name}
+                      onClick={handleCloseNavMenu}
+                      sx={{ my: 2, color: "white", display: "block" }}
+                    >
+                      {name}
+                    </Button>
+                  </Link>
+                ))}
+              </Box>
+
+              <Box sx={{ flexGrow: 0 }}>
+                <Tooltip title="Open settings">
+                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                    <Avatar alt="Remy Sharp" />
+                  </IconButton>
+                </Tooltip>
+                <Menu
+                  sx={{ mt: "45px" }}
+                  id="menu-appbar"
+                  anchorEl={anchorElUser}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  open={Boolean(anchorElUser)}
+                  onClose={handleCloseUserMenu}
+                >
+                  {settings.map(({ name, route }) => (
+                    <Link
+                      key={name}
+                      href={`${name !== "Perfil" ? route : `${route}/1`}`}
+                    >
+                      <MenuItem key={route} onClick={handleCloseUserMenu}>
+                        <Typography textAlign="center">{name}</Typography>
+                      </MenuItem>
+                    </Link>
+                  ))}
+                </Menu>
+              </Box>
+            </Toolbar>
+          </Container>
+        </AppBar>
+      )}
+    </>
+  );
 };
--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,27 +1,27 @@
-'use client';
-import { Container } from '@mui/material';
-import { Hero } from './common/components';
-import { EventCard } from './common/components/EventCard';
-import SliderContainer from './pages/home/components/SliderContainer';
+"use client";
+import { Container } from "@mui/material";
+import { Hero } from "./common/components";
+import { EventCard } from "./common/components/EventCard";
+import SliderContainer from "./pages/home/components/SliderContainer";
 
 const sectionArrays = [
-  { title: 'Panoramas Hoy' },
-  { title: 'Destacados' },
-  { title: 'Próximos 7 días' },
+  { title: "Panoramas Hoy" },
+  { title: "Destacados" },
+  { title: "Próximos 7 días" },
 ];
 
 export default function Home() {
   return (
     <main>
-      <Container maxWidth="lg" sx={{ mt: '3rem', mb: '3rem' }}>
+      <Container maxWidth="lg" sx={{ mt: "3rem", mb: "3rem" }}>
         <Hero />
         {sectionArrays.map((item) => {
           return (
-            <SliderContainer sliderTitle={item.title}>
+            <SliderContainer key={item.title} sliderTitle={item.title}>
               {Array(4)
                 .fill(0)
                 .map((item) => {
-                  return <EventCard key={item} />;
+                  return <EventCard key={item.title} />;
                 })}
             </SliderContainer>
           );
