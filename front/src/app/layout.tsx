13d21f2a714cd609c3f394c3eead0848c35a8904
Fernanda Aguilar Pfannschmidt <fa.aguilarpfa@gmail.com>
2023-06-02 18:50:37 -0400

    fix: navbar

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -15,9 +15,6 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <Head>
-                <title>NeoVest SignIn</title>
-            </Head>
             {/* <Providers> */}
             <body>
                 <Providers>
